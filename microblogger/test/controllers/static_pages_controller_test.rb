34907f578f366bc2ca3dcf941394a50ffc151286
mpilcher90 <mpilcher90@gmail.com>
2015-12-14 09:58:14 +0000

    add contact page

--- microblogger/test/controllers/static_pages_controller_test.rb
+++ microblogger/test/controllers/static_pages_controller_test.rb
@@ -18,5 +18,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Microblogger"
   end
-
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Microblogger"
+  end
 end
