d0d3d46513193782fa2eb4e17871624bfe0f06cb
mpilcher90 <mpilcher90@gmail.com>
2016-01-07 10:02:04 +0000

    add login form

--- members-only/config/routes.rb
+++ members-only/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
   get 'about'   => 'static_pages#about'
   get 'signup'  => 'users#new'
+  get 'login'   => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   
   resources :users
 
