144966cebd716116476dc59de8caa9ae96c2beba
mpilcher90 <mpilcher90@gmail.com>
2016-01-09 04:51:59 +0000

    add authorization for users

--- members-only/app/controllers/users_controller.rb
+++ members-only/app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index]
+  
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
   end
@@ -21,5 +27,12 @@ class UsersController < ApplicationController
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please login to to continue"
+        redirect_to login_url
+      end
     end
 end
