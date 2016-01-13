57881af0f4316788416d2d5c787891eb5e0edcf3
mpilcher90 <mpilcher90@gmail.com>
2016-01-13 13:41:09 +0000

    add post form

--- members-only/app/controllers/articles_controller.rb
+++ members-only/app/controllers/articles_controller.rb
@@ -1,12 +1,27 @@
 class ArticlesController < ApplicationController
   
   before_action :logged_in_user, only: [:index]
+  #before_action :admin_user, only: [:create]
   
   def index
     @articles = Article.all
   end
   
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def new
+    @article = Article.new
+  end
+  
+  def create 
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
   
   def show
@@ -16,12 +31,22 @@ class ArticlesController < ApplicationController
   def edit
   end
   
+  def destroy
+  end
+  
   private
-    
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+  
     def logged_in_user
       unless logged_in?
         flash[:danger] = "Please login to to continue"
         redirect_to login_url
       end
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
