13116c71a9146210f172165a24f0f5d8f45f67ca
mpilcher90 <mpilcher90@gmail.com>
2016-01-16 11:18:02 +0000

    add kitten display and create

--- basic-api/app/controllers/kittens_controller.rb
+++ basic-api/app/controllers/kittens_controller.rb
@@ -1,18 +1,28 @@
 class KittensController < ApplicationController
   def index
+    @kittens = Kitten.all
   end
   
   def show
+    @kitten = Kitten.find(params[:id])
   end
   
   def new
+    @kitten = Kitten.new
   end
   
   def create
+    @kitten = Kitten.create(kitten_params)
   end
   
   def edit
   end
+  
+  private
+    #Strong parameters
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :color)
+    end
   
   
 end
