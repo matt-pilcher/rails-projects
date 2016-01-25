d9f9e8296aed149b9336be83cd2e7ba4e352c6f7
mpilcher90 <mpilcher90@gmail.com>
2016-01-25 10:27:23 +0000

    add update action

--- social-network/app/controllers/users_controller.rb
+++ social-network/app/controllers/users_controller.rb
@@ -19,6 +19,21 @@ class UsersController < ApplicationController
   end
   
   def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Updated!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
   
   private
