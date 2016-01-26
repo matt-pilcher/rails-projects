a25c4eb3103276da5b101172f3e185fee9c606f6
mpilcher90 <mpilcher90@gmail.com>
2016-01-26 07:03:48 +0000

    add login/logout functionality

--- social-network/config/routes.rb
+++ social-network/config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   
   resources :users
   
+  get 'home' => 'static_pages#home'
+  
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
