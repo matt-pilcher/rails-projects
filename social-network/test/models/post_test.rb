c9c83f40c2036ec3dd88596fe2e55867bad0b7f8
mpilcher90 <mpilcher90@gmail.com>
2016-01-28 08:01:02 +0000

    add tests for user posts

--- social-network/test/models/post_test.rb
+++ social-network/test/models/post_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:matt)
+    @post = @user.posts.build(content: "Lorem ipsum")
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+  
+  test "content should be present" do
+    @post.content = "   "
+    assert_not @post.valid?
+  end
+
+  test "content should be no more than 5000 characters" do
+    @post.content = "a" * 5001
+    assert_not @post.valid?
+  end
+
 end
