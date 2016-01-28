4a8153e75da8a493f98aa5b4f1f666f35286ee4a
mpilcher90 <mpilcher90@gmail.com>
2016-01-28 13:48:24 +0000

    add user status and styling

--- social-network/app/helpers/users_helper.rb
+++ social-network/app/helpers/users_helper.rb
@@ -1,8 +1,9 @@
 module UsersHelper
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=220"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=299"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
     
     #Code for custom default gravatar image
     
@@ -10,5 +11,4 @@ module UsersHelper
     #gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     #gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=200&d=#{CGI.escape(default_url)}"
     #image_tag(gravatar_url, alt: user.name, class: "gravatar")
-  end
 end
--- social-network/db/seeds.rb
+++ social-network/db/seeds.rb
@@ -6,3 +6,9 @@ User.create(name: "User2", email: "user2@example.com", password: "password" )
 User.create(name: "User3", email: "user3@example.com", password: "password" )
 User.create(name: "User4", email: "user4@example.com", password: "password" )
 User.create(name: "User5", email: "user5@example.com", password: "password" )
+
+users = User.order(:created_at).take(6)
+10.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.posts.create!(content: content) }
+end
