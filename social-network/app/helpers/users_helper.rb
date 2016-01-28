d5e90291f71da59ceacb556e0ba8df7ad4889685
mpilcher90 <mpilcher90@gmail.com>
2016-01-28 09:36:37 +0000

    add basic profile styling

--- social-network/app/helpers/users_helper.rb
+++ social-network/app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=220"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    
+    #Code for custom default gravatar image
+    
+    #default_url = "#{root_url}/images/Avatar.png"
+    #gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    #gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=200&d=#{CGI.escape(default_url)}"
+    #image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
--- social-network/app/models/user.rb
+++ social-network/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, dependent: :destroy #destroys the posts when the user is deleted
   validates :name, presence: true, length: { maximum: 50 }
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
