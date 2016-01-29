df083b98ba7905c1fef28ce8fbc06667a4a179e6
mpilcher90 <mpilcher90@gmail.com>
2016-01-29 07:38:32 +0000

    add destroy method for posts

--- social-network/app/controllers/posts_controller.rb
+++ social-network/app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @post = current_user.posts.build(post_params)
@@ -7,16 +8,26 @@ class PostsController < ApplicationController
       flash[:success] = "Status updated!"
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
   
   def destroy 
+    @post.destroy
+    flash[:success] = "Post deleted"
+    #request.referrer returns to the previous url
+    redirect_to request.referrer || root_url 
   end
   
   private
     
     def post_params
       params.require(:post).permit(:content)
+    end
+    
+    def correct_user
+      @post = current_user.posts.find_by(id: params[:id])
+      redirect_to root_url if @post.nil?
     end
 end
