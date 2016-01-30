eda44fd9a917938c3bbbfd3a325947b9c84e5150
mpilcher90 <mpilcher90@gmail.com>
2016-01-30 09:10:58 +0000

    finish image validations

--- social-network/app/uploaders/picture_uploader.rb
+++ social-network/app/uploaders/picture_uploader.rb
@@ -1,6 +1,18 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
+  
+  #This will keep the uploaded images from breaking the site layout
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+  
+  #Use cloud storage for production enviroment
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -38,9 +50,9 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  #def extension_white_list
-     # %w(jpg jpeg gif png)
-  #end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
