33338f0d9852e2f40016a6d70c2b6644a3ad552e
mpilcher90 <mpilcher90@gmail.com>
2016-05-23 11:20:22 +0000

    associate users with posts and prevent them from editing and destroying other users posts

--- Socializer/app/controllers/posts_controller.rb
+++ Socializer/app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :protect_post, only: [:edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -9,11 +11,12 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
-    @post = Post.create(post_params)
+    @post = current_user.posts.build(post_params)
+    
     if @post.save
       redirect_to posts_path
     else
@@ -49,6 +52,13 @@ class PostsController < ApplicationController
     
     def set_post
       @post = Post.find(params[:id]) # Removing repeated code with this method
+    end
+    
+    def post_protect
+      unless current_user == @post.user
+        flash[:alert] = "Unauthorized"
+        redirect_to posts_path
+      end
     end
 end  
 
