fd9d25ce83127a0aad14fffc81a4ed71f1fc24d2
mpilcher90 <mpilcher90@gmail.com>
2016-05-23 09:43:52 +0000

    finish user authentication test

--- Socializer/spec/features/user_authentication_spec.rb
+++ Socializer/spec/features/user_authentication_spec.rb
@@ -18,5 +18,24 @@ feature 'User authentication' do
     expect(page).to have_content('Logout')
   end
   
+  scenario 'can logout' do
+    visit '/'
+    click_link 'Login'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'foobar'
+    
+    click_button 'Log in'
+    click_button 'Logout'
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
   
+  scenario 'cannot view posts without logging in' do
+    visit posts_path
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
+    
+  scenario 'cannot create a new post without logging in' do
+    visit new_post_path
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
 end
