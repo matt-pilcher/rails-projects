39ebd7d99f640526a9904cf752f446dc70008923
mpilcher90 <mpilcher90@gmail.com>
2016-05-24 07:48:17 +0000

    add test for creating comments

---
+++ Socializer/spec/features/creating_comments_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature 'Posting comments' do
+  background do
+    user = create(:user)
+    job = create(:post, user_id: user.id)
+    
+    visit '/'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+  end
+  
+  scenario 'can leave a comment' do
+    visit '/'
+    fill_in "comment_content_#{post.id}", with: "My insightful comment"
+    click_button 'Send'
+    
+    expect(page).to have_content('My insightful comment')
+  end
+end
