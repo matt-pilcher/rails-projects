a0ac667fbb64142af0528ec60eadf7a9d3526c23
mpilcher90 <mpilcher90@gmail.com>
2016-05-25 08:51:36 +0000

    add a working comment form and a test for deleting comments

--- Socializer/spec/factories/comments.rb
+++ Socializer/spec/factories/comments.rb
@@ -2,6 +2,5 @@ FactoryGirl.define do
   factory :comment do
     user nil
     post nil
-    content "MyText"
   end
 end
---
+++ Socializer/spec/features/deleting_comments_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'Deleting comments' do 
+  background do
+    user = create :user
+    job = create(:post, user_id: user.id)
+    
+    visit '/'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+  end
+  
+  feature 'can delete a comment' do
+    visit '/'
+    fill_in "comment_content_#{post.id}", with: "My insightful comment"
+    click_button 'Send'
+    
+    expect(page).to have_content('My insightful comment')
+    click_link 'delete'
+    expect(page).to_not have_content('My insightful comment')
+  end
+end
